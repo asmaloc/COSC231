90cdb0569439c17843c0271597e1d670156ca6f2
asmaloc <amalocha@emich.edu>
2016-10-11 14:57:50 -0400

    ...

---
+++ join.js
@@ -0,0 +1,12 @@
+if (typeof localStorage == 'undefined')
+{
+  alert("Local storage is not available")
+}
+else
+{
+  var username = document.getElementById()
+  function store() {
+      localStorage.setItem('username', name.value);
+      localStorage.setItem('pw', pw.value);
+  }
+}
